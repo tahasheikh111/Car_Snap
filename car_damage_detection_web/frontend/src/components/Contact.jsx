c522d9839271e83e5692ac4913a44dce58cdb0ae
Rameez <137998246+Rameez087@users.noreply.github.com>
2024-04-25 15:30:18 +0500

    ..

--- car_damage_detection_web/frontend/src/components/Contact.jsx
+++ car_damage_detection_web/frontend/src/components/Contact.jsx
@@ -1,44 +1,44 @@
-import React from 'react';
-import Footer from "./Footer.jsx";
-import { FaFacebook, FaInstagram, FaLinkedin, FaYoutube, FaArrowRight } from 'react-icons/fa';
-import '../styles/contactus.css'; // Import the CSS file
-import imagepath from "../images/dreamcar.jpg";
-import BlackNav from "./BlackNav.jsx";
+// import React from 'react';
+// import Footer from "./Footer.jsx";
+// import { FaFacebook, FaInstagram, FaLinkedin, FaYoutube, FaArrowRight } from 'react-icons/fa';
+// import '../styles/contactus.css'; // Import the CSS file
+// import imagepath from "../images/dreamcar.jpg";
+// import BlackNav from "./BlackNav.jsx";
 
-const socialMediaAccounts = [
-  { name: 'Facebook' , icon: <FaFacebook size={36} color="#4267B2" />, url: 'https://www.facebook.com/yourpage' },
-  { name: 'Instagram', icon: <FaInstagram size={36} color="#E1306C" />, url: 'https://www.instagram.com/yourpage' },
-  { name: ' LinkedIn', icon: <FaLinkedin size={36} color="#0A66C2" />, url: 'https://www.linkedin.com/yourpage' },
-  { name: '  YouTube', icon: <FaYoutube size={36} color="#FF0000" />, url: 'https://www.youtube.com/yourpage' },
-];
+// const socialMediaAccounts = [
+//   { name: 'Facebook' , icon: <FaFacebook size={36} color="#4267B2" />, url: 'https://www.facebook.com/yourpage' },
+//   { name: 'Instagram', icon: <FaInstagram size={36} color="#E1306C" />, url: 'https://www.instagram.com/yourpage' },
+//   { name: ' LinkedIn', icon: <FaLinkedin size={36} color="#0A66C2" />, url: 'https://www.linkedin.com/yourpage' },
+//   { name: '  YouTube', icon: <FaYoutube size={36} color="#FF0000" />, url: 'https://www.youtube.com/yourpage' },
+// ];
 
-const ContactPage = () => {
-  return (
-    <div className="contact-page" style={{ backgroundImage: `url(${imagepath})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
+// const ContactPage = () => {
+//   return (
+//     <div className="contact-page" style={{ backgroundImage: `url(${imagepath})`, backgroundSize: 'cover', backgroundPosition: 'center' }}>
       
-      <header>
-        <h1>Contact CarSnap</h1>
-      </header>
-      <BlackNav />
-      {/* Main content */}
-      <div className="main-content">
+//       <header>
+//         <h1>Contact CarSnap</h1>
+//       </header>
+//       <BlackNav />
+//       {/* Main content */}
+//       <div className="main-content">
     
-        {socialMediaAccounts.map((account) => (
-          <div key={account.name} className="social-media-item">
-            <div className="icon">{account.icon}</div>
-            <div className="name">{account.name}</div>
-            <div className="button">
-            <a href={account.url} target="_blank" rel="noopener noreferrer">
-              <button><FaArrowRight /></button>
-            </a>
-            </div>
-          </div>
-        ))}
-      </div>
+//         {socialMediaAccounts.map((account) => (
+//           <div key={account.name} className="social-media-item">
+//             <div className="icon">{account.icon}</div>
+//             <div className="name">{account.name}</div>
+//             <div className="button">
+//             <a href={account.url} target="_blank" rel="noopener noreferrer">
+//               <button><FaArrowRight /></button>
+//             </a>
+//             </div>
+//           </div>
+//         ))}
+//       </div>
 
-      <Footer />
-    </div>
-  );
-};
+//       <Footer />
+//     </div>
+//   );
+// };
 
-export default ContactPage;
+// export default ContactPage;
